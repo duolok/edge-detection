17f44c3791e70d6879bdbc8b90b8ea4909aa46c9
dusanlecic <dusan.lecic5@gmail.com>
2023-05-23 12:01:32 +0200

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
-#include <vector>
-#include <stdlib.h>
-#include <string>
-#include "bitmap/BitmapRawConverter.h"
-#include "bitmap/EasyBMP.h"
-#include <tbb/task_group.h>
 #include "detector/detector.h"
 
-using namespace std;
-using namespace tbb;
-
-int main(int argc, char * argv[])
+int main()
 {
     Detector d;
     d.start_detector();
